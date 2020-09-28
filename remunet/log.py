02fb4ae7280853ce48a0a68aae2e54113e71a399
Anton <akachanov1997@gmail.com>
2020-09-28 21:22:02 +0000

    move containernet code to remunet module

---
+++ remunet/log.py
@@ -0,0 +1,24 @@
+"Logging functions for Mininet."
+
+from mininet.log import *
+
+
+
+class RemunetLogger(MininetLogger, object):
+
+    def _log(self, level, msg, args, exc_info=None, extra=None, stack_info=False):
+        super()._log(level, msg, args, exc_info, extra, stack_info)
+
+        print("--->", msg)
+
+
+lg = RemunetLogger()
+
+
+_loggers = lg.info, lg.output, lg.warning, lg.error, lg.debug
+_loggers = tuple(makeListCompatible(logger)
+                 for logger in _loggers)
+lg.info, lg.output, lg.warning, lg.error, lg.debug = _loggers
+info, output, warn, error, debug = _loggers
+
+setLogLevel = lg.setLogLevel
